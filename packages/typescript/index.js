3a71a0f16709f4b55ba9f3f2e44ca01f8fbb8418
Kirk Lin <linkirk@163.com>
2022-10-12 20:01:50 +0800

    chore: @typescript-eslint/member-delimiter-style: semi

--- packages/typescript/index.js
+++ packages/typescript/index.js
@@ -17,7 +17,7 @@ module.exports = {
 
     // TS
     "@typescript-eslint/ban-ts-comment": ["error", { "ts-ignore": "allow-with-description" }],
-    "@typescript-eslint/member-delimiter-style": ["error", { multiline: { delimiter: "none" } }],
+    "@typescript-eslint/member-delimiter-style": ["error", { multiline: { delimiter: "semi" } }],
     "@typescript-eslint/type-annotation-spacing": ["error", {}],
     "@typescript-eslint/consistent-type-imports": ["error", { prefer: "type-imports", disallowTypeAnnotations: false }],
     "@typescript-eslint/consistent-type-definitions": ["error", "interface"],
