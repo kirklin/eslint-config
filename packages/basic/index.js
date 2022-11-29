9bfaba873d1d84831de530c652bb16b472572a7e
Kirk Lin <linkirk@163.com>
2022-11-29 16:27:59 +0800

    feat(basic): add max-statements-per-line

--- packages/basic/index.js
+++ packages/basic/index.js
@@ -283,6 +283,7 @@ module.exports = {
     "require-await": "off",
     "no-return-assign": "off",
     "operator-linebreak": ["error", "before"],
+    "max-statements-per-line": ["error", { max: 1 }],
 
     // unicorns
     // Pass error message when throwing errors
