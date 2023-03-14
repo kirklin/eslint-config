0bc700eceded99ea2b6f79a40d3a29180ca4bf23
Kirk Lin <linkirk@163.com>
2023-03-14 11:18:35 +0800

    fix: disable vue/no-v-text-v-html-on-component

--- packages/vue/index.js
+++ packages/vue/index.js
@@ -36,6 +36,7 @@ module.exports = {
     "vue/require-default-prop": "off",
     "vue/multi-word-component-names": "off",
     "vue/prefer-import-from-vue": "off",
+    "vue/no-v-text-v-html-on-component": "off",
 
     // reactivity transform
     "vue/no-setup-props-destructure": "off",
@@ -61,7 +62,6 @@ module.exports = {
     }],
     "vue/no-restricted-v-bind": ["error", "/^v-/"],
     "vue/no-useless-v-bind": "error",
-    "vue/no-v-text-v-html-on-component": "error",
     "vue/padding-line-between-blocks": ["error", "always"],
     "vue/prefer-separate-static-class": "error",
 
