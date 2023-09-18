fbbf9066e1e1e2fba0a32f000af0c46da8ddbf01
Kirk Lin <linkirk@163.com>
2023-09-18 17:57:12 +0800

    fix(vue): only use eslint in JavaScript

--- packages/eslint-config-vue/index.js
+++ packages/eslint-config-vue/index.js
@@ -12,7 +12,7 @@ module.exports = {
       files: ["*.vue"],
       parser: "vue-eslint-parser",
       parserOptions: {
-        parser: "@typescript-eslint/parser",
+        parser: TS ? "@typescript-eslint/parser" : null,
       },
       rules: {
         "no-unused-vars": "off",
