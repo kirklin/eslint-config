e77a4c2a390603969c136df78debb4465f891c09
Kirk Lin <linkirk@163.com>
2025-09-01 15:52:11 +0800

    chore: update deps

--- src/cli/constants-generated.ts
+++ src/cli/constants-generated.ts
@@ -1,9 +1,9 @@
 export const versionsMap = {
-  "@eslint-react/eslint-plugin": "^1.52.3",
-  "@next/eslint-plugin-next": "^15.4.6",
-  "@unocss/eslint-plugin": "^66.4.2",
+  "@eslint-react/eslint-plugin": "^1.52.9",
+  "@next/eslint-plugin-next": "^15.5.2",
+  "@unocss/eslint-plugin": "^66.5.0",
   "astro-eslint-parser": "^1.2.2",
-  "eslint": "^9.33.0",
+  "eslint": "^9.34.0",
   "eslint-plugin-astro": "^1.3.1",
   "eslint-plugin-format": "^1.0.1",
   "eslint-plugin-react-hooks": "^5.2.0",
